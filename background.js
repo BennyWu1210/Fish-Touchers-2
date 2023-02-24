c31aa6495c9f313cf3ce7c14ef3b5e46a5423e4c
icsbillwei <shuheng.wei@outlook.com>
2023-02-24 18:19:27 -0500

    dalao nb

---
+++ background.js
@@ -0,0 +1,10 @@
+function become_dalao() {
+    document.getElementById("button").addEventListener("click", myFunction);
+}
+
+function myFunction() {
+    let dalao = document.getElementById("button");
+    dalao.innerHTML = "Bill is dalao";
+}
+
+become_dalao();
